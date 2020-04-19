8f271178c36678c37ea4dec73482873625eaf118
yangshihao-arron <2867351265@qq.com>
2020-04-19 18:16:39 +0800

    second change

--- src/array/Array.java
+++ src/array/Array.java
@@ -1,12 +1,12 @@
 package array;
 
-public class Array {
-    private int[] data;
+public class Array <E> {
+    private E[] data;
     private int size;
 
     //传入capacity
     public Array(int capacity){
-        data = new int[capacity];
+        data = (E[]) new Object[capacity];
     }
 
     //capacity = 10
@@ -26,7 +26,7 @@ public class Array {
         return size == 0;
     }
     //向数组末尾添加元素e
-    public void addLast(int e){
+    public void addLast(E e){
 //        if(size == data.length)
 //            throw new IllegalArgumentException("addLast faileed. array is full");
 //        data[size] = e;
@@ -34,15 +34,15 @@ public class Array {
         add(size,e);
     }
     //向数组首部添加元素e
-    public void addFirst(int e){
+    public void addFirst(E e){
         add(0,e);
     }
     //向数组index位置插入元素e
-    public void add(int index,int e ){
-        if(size == data.length)
-            throw new IllegalArgumentException("addLast failed. array is full");
+    public void add(int index,E e ){
         if(index < 0 || index > size)//index = size说明只用移动一个元素
             throw new IllegalArgumentException("add failed. index is not available");
+        if(size == data.length)
+            resize(2*data.length);
         for(int i = size-1; i >=index; i--){
             data[i+1] = data[i];
         }
@@ -51,55 +51,58 @@ public class Array {
     }
 
     //获取index索引位置的元素
-    int get(int index){
+    public E get(int index){
         if(index < 0 | index >= size)  //index == size 处索引为空
             throw new IllegalArgumentException(("Get failed,Index is illegal"));
         return data[index];
     }
     //修改index索引位置的元素为e
-    void set(int index,int e){
+    void set(int index,E e){
         if(index< 0 || index >= size)
             throw  new IllegalArgumentException("Set failed,index is Illegal");
             data[index] = e;
     }
     //查找数组中是否有元素e
-    public boolean contains(int e){
+    public boolean contains(E e){
         for(int i = 0; i < size;i++){
-            if(data[i] == e)
+            if(data[i] .equals(e) )
                 return true;
         }
         return false;
     }
     //找不到返回-1，找到返回e的索引
-    public int find(int e){
+    public int find(E e){
         for(int i = 0; i < size;i++){
-            if(data[i] == e)
+            if(data[i] .equals(e) )
                 return i;
         }
         return -1;
     }
     //从数组中删除Index位置的元素，返回删除的元素
-    public int remove(int index){
+    public E remove(int index){
         if(index < 0 || index >=size)
         throw new IllegalArgumentException("remove failed, Index is illegal");
-        int ret = data[index];
+        E ret = data[index];
         for(int i = index + 1; i < size; i++)
             data[i-1] = data[i];
-        size--;
+        size--;//存在对象引用，不会被垃圾机智回收
+        data[size] = null; //loitering objects != memory leak
+        if(size == data.length / 4)
+            resize(data.length / 2);
         return ret;
     }
 
     //从数组中删除第一个元素，返回删除的元素
-    public int removeFirst(){
+    public E removeFirst(){
         return remove(0);
     }
 
     //从数组中删除最后位置的元素，返回删除的元素
-    public int removeLast(){
+    public E removeLast(){
        return remove(size-1);
     }
     //从数组中删除元素e
-    public void removeElement(int e){
+    public void removeElement(E e){
         int index = find(e);
         if(index != -1)
             remove(index);
@@ -107,7 +110,7 @@ public class Array {
     @Override
     public String toString(){
       StringBuilder res = new StringBuilder();
-      res.append(String.format("Array:size = %d, capacity = %d/n",size,data.length));
+      res.append(String.format("Array:size = %d, capacity = %d\n",size,data.length));
       res.append('[');
       for(int i = 0 ; i < size; i++ ){
           res.append(data[i]);
@@ -118,5 +121,11 @@ public class Array {
       res.append(']');
       return  res.toString();
     }
-
+    private void resize(int newCapacity){
+        E[] newData = (E[]) new Object[newCapacity];
+        for(int i = 0 ; i < size; i++){
+            newData[i] = data[i];
+        }
+        data = newData;
+    }
 }
