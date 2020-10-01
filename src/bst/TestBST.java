13a21c1ca2be744035dfbf4ab3d10d5b8b3843df
yangshihao-arron <2867351265@qq.com>
2020-10-01 09:09:05 +0800

    Recurisf

--- src/bst/TestBST.java
+++ src/bst/TestBST.java
@@ -1,15 +1,25 @@
 package bst;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class TestBST {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         BST<Integer> bst = new BST<Integer>();
-        bst.add(20);
-        bst.add(18);
-        bst.add(24);
-        bst.add(15);
-        System.out.println(bst.contains(18));
-        bst.preOrder();
-        System.out.println();
-        System.out.println(bst);
-  }
+        int n = 1000;
+        Random random = new Random();
+
+        for(int i= 0; i < n ;i++){
+            bst.add(random.nextInt(10000));
+        }
+        ArrayList<Integer> nums = new ArrayList<>();
+        while(!bst.isEmpty()){
+            nums.add(bst.removeMax());
+        }
+        for(int i = 1; i < nums.size(); i++){
+            if(nums.get(i-1) < nums.get(i))
+                System.out.println("false");
+        }
+    }
+
 }
