90f4d53c4ef34781c325f5cecb816d4c41c026d4
yangshihao-arron <2867351265@qq.com>
2020-10-28 15:39:58 +0800

    add maxheap

---
+++ src/test/TestA.java
@@ -0,0 +1,11 @@
+package test;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class TestA {
+    public static void main(String[] args) {
+        List<Integer> list = new LinkedList<>();
+
+    }
+}
