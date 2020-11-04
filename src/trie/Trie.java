762ce69a94f691036325570dcb5d361c5a7de0b0
yangshihao-arron <2867351265@qq.com>
2020-11-04 15:46:10 +0800

    add trie

---
+++ src/trie/Trie.java
@@ -0,0 +1,73 @@
+package trie;
+
+import java.util.TreeMap;
+
+public class Trie {
+
+    private class Node{
+
+        public boolean isWord;
+        public TreeMap<Character,Node> next;
+
+        public Node(boolean isWord){
+            this.isWord = isWord;
+            next = new TreeMap<>();
+        }
+        public Node(){
+            this(false);
+        }
+    }
+
+    private  Node root;
+    private int size;
+
+    public Trie(){
+        root = new Node();
+        size = 0;
+    }
+
+    public int getSize(){
+        return size;
+    }
+
+    public void add(String word){
+
+        Node cur = root;
+        for(int i = 0; i < word.length(); i++){
+            char c = word.charAt(i);
+            if(cur.next.get(c) == null){
+                cur.next.put(c,new Node());
+            }
+            cur = cur.next.get(c);
+        }
+        if(!cur.isWord){
+            cur.isWord = true;
+            size ++;
+        }
+    }
+
+
+    public boolean contains(String word){
+
+        Node cur = root;
+        for(int i = 0; i <word.length(); i ++){
+            char c = word.charAt(i);
+            if(cur.next.get(c) == null)
+                return false;
+            cur = cur.next.get(c);
+        }
+        return cur.isWord;
+    }
+
+    public boolean isPrefix(String prefix){
+
+        Node cur = root;
+        for(int i = 0; i < prefix.length(); i++){
+            char c = prefix.charAt(i);
+            if(cur.next.get(c) == null)
+                return false;
+            cur = cur.next.get(c);
+        }
+        return true;
+    }
+}
