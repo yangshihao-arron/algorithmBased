1b6340cdd82598808c3aff3c20f6fab4e24e73e1
yangshihao-arron <2867351265@qq.com>
2022-03-09 21:17:35 +0800

    test

--- src/redbalcktree/Main.java
+++ src/redbalcktree/Main.java
@@ -6,7 +6,6 @@ import java.util.Collections;
 public class Main {
 
     public static void main(String[] args) {
-
         System.out.println("Pride and Prejudice");
 
         ArrayList<String> words = new ArrayList<>();
